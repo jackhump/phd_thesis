f4236c475c9a36d916ef9a3dc186a713601660e3
Jack Humphrey <jack@jacks-mbp.home>
2018-08-28 13:28:43 +0100

    added thesis_full.tex to build entire thesis

--- build_thesis.sh
+++ build_thesis.sh
@@ -1,6 +1,14 @@
+
 rm *.out *.aux *.toc *.lot *.blg *.log *.lof *.bbl *.pdf
-pdflatex thesis_master
-bibtex thesis_master
-pdflatex thesis_master
-pdflatex thesis_master
-open thesis_master.pdf
+
+if [ $1 == "full" ]; then
+	thesis=thesis_full
+else
+	thesis=thesis_master
+fi
+
+pdflatex $thesis
+bibtex $thesis
+pdflatex $thesis
+pdflatex $thesis
+open ${thesis}.pdf
